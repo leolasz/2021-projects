627454b20a43ea4acd3875cd44536abec5f6694e
Leonardo Souza <leonardosouza@192-168-1-106.tpgi.com.au>
2021-02-24 20:28:05 +1100

    const e var

---
+++ JScript/Fundamentos/comentarios.js
@@ -0,0 +1,17 @@
+//comentarios
+console.log("linha1")
+
+/*
+comentario de linhas 
+multiplas linhas
+*/
+
+console.log('linha 2')
+
+/*
+*comentario de 
+*multiplas linhas
+*/
+console.log('linha 3')
+
+console.log('nada!!')
